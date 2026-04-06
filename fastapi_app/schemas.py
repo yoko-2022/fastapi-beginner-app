eee61108828bbdbc470d6881e9e45a24aed297c7
yoko-2022 <rilapy_029@yahoo.co.jp>
2026-04-06 16:59:24 +0900

    バリデーションを追加

---
+++ fastapi_app/schemas.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, Field
+from datetime import date
+from typing import Optional
+
+# スキーマの定義
+class TaskBase(BaseModel):
+    # タイトルは必須項目とし、入力文字数は1～10文字
+    title: str = Field(..., min_length=1, max_length=10)
+    # 詳細の入力文字数は最大100文字まで・省略するとNull
+    description: Optional[str] = Field(None, max_length=100)
+    # 締切日はYYYY-MM-DD形式・省略するとNull
+    deadline: Optional[date] = None
+    # 完了フラグはデフォルト値がfalse
+    complete: bool = False
+
+# 新規作成時に使うバリデーション（TaskBaseを継承）
+class TaskCreate(TaskBase):
+    pass
+
+# 取得時に使うバリデーション
+class Task(TaskBase):
+    id: int
+
+# PydanticがSQLAlchemyのオブジェクトをJSONに変換
+    class Config:
+        from_attributes=True
